7c4701e76973b55b899394f255ead0cd28b6b413
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-06-20 13:52:35 +0300

    All Movies Clean Code

--- src/Pages/AllMovies.js
+++ src/Pages/AllMovies.js
@@ -5,34 +5,36 @@ import Pagination from "../Components/Utils/Pagination"
 import Card from "../Components/MovieCard"
 import { getMovies } from "../api";
 
+const MAX_PAGES = 500;
+
 export default function AllMovies() {
 
   const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
 
-  function dispatchPage(page) {
-    dispatch(getMovies(page))
+  useEffect(() => { dispatch(getMovies(1)) }, [dispatch]);
+
+  function renderMovies() {
+    if (movies.loading) return <SimpleSpinner title='جارى تحميل الافلام' />
+    if (movies.error) return <h2 className="text-center">خطأ فى التحميل</h2>
+
+    const moviesResults = movies.data.results;
+
+    if (moviesResults && moviesResults.length > 0)
+      return (
+        <>
+          <div className="list mb-4"> {moviesResults.map(movie => <Card key={movie.id} movie={movie} />)} </div>
+          <Pagination getPage={page => dispatch(getMovies(page))} currentPage={movies.data.page} totalPages={MAX_PAGES} />
+        </>
+      )
+
+    if (moviesResults && moviesResults.length === 0)
+      return <h2 className="text-center">لا يوجد أفلام</h2>;
   }
-
-  // eslint-disable-next-line
-  useEffect(() => { dispatchPage(1); }, []);
-
-  let results = <>
-    {movies.data.results ?
-      <>
-        <div className="list mb-4">
-          {movies.data.results.map(movie => <Card key={movie.id} movie={movie} />)}
-        </div>
-        <Pagination getPage={dispatchPage} currentPage={movies.data.page} totalPages={500} />
-      </>
-      :
-      movies.error && <h2 className="text-center">لا يوجد أفلام</h2>
-    }
-  </>
 
   return (
     <div className="container py-4">
-      {movies.loading ? <SimpleSpinner title='جارى تحميل الافلام' /> : results}
+      {renderMovies()}
     </div>
   )
 }
