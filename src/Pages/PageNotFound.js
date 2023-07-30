6b11ef048ba256aa41dc7ecaa9baa6531f62aba1
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-07-30 11:28:34 +0300

    Error Page Updated

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -1,8 +1,10 @@
+import error from "../Imgs/404 Error.svg";
+
 export default function PageNotFound() {
   return (
-    <div className='text-center my-5'>
-      <h1 className='fw-bold' style={{ fontSize: '7em' }}>404</h1>
+    <div className="text-center my-5">
+      <img src={error} alt="Error 404" style={{ width: "375px", maxWidth: "100%", margin: "5px" }} />
       <h2>عذرا الصفحة غير موجودة</h2>
     </div>
-  )
+  );
 }
